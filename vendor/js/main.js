2e50fd9314c6663d934a89b3ae3e4a9b9d923d82
unknown <nguyenvivo2000@gmail.com>
2021-04-26 21:34:19 +0700

    --- Auto-Git Commit ---

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var header = document.querySelector('.header');
     var button_cricle = document.querySelector('.button-cricle');
+    var icon_sun = document.querySelector('.icon-dark--img');
     window.addEventListener('scroll', function() {
         if (window.pageYOffset > 10) {
             header.classList.add('header-scroll');
@@ -12,6 +13,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
         }
     });
+    icon_sun.onclick = function() {
+        document.body.classList.toggle("dark-theme");
+        if (document.body.classList.contains("dark-theme")) {
+            icon_sun.src = "vendor/img/sun.png";
 
+        } else {
+            icon_sun.src = "vendor/img/moon.png";
+
+        }
+    }
 
 })
